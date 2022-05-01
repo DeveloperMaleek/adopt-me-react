8e68d32932bb09f658ccae32cc6d2119e9188a32
Malik Kolade <kolademalikademola@gmail.com>
2022-05-01 20:55:31 +0000

    begin intro to react-5 and setup component done

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// you code is going to be here , for now
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
+    ])
+}
+
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havannes" }),
+        React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Hungary" }),
+        React.createElement(Pet, { name: "Doink", animal: "Fish", breed: "Japaneese" }),
+    );
+};
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById("root")
+);
